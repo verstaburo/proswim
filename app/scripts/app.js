67fe5054c026d827481e59f8961092d973868cff
Алексей Богословский <bogoslovskiya@gmail.com>
2018-02-13 19:15:17 +0300

    add global options

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,13 @@ import objectFitImages from 'object-fit-images';
 import anchor from '../blocks/js-functions/anchor';
 import { freezebuttons } from '../blocks/js-functions/freeze';
 import scrollanimation from '../blocks/js-functions/scrollanimation';
-import { selects, sliders, datepicker, inputmask, numberinput } from '../blocks/form-elements/form-elements';
+import {
+  selects,
+  sliders,
+  datepicker,
+  inputmask,
+  numberinput
+} from '../blocks/form-elements/form-elements';
 import popups from '../blocks/popups/popups';
 import scrollbar from '../blocks/scrollbar/scrollbar';
 import slider from '../blocks/slider/slider';
@@ -15,6 +21,18 @@ import '../blocks/accordion/accordion';
 import '../blocks/dropdown/dropdown';
 
 const $ = window.$;
+
+window['globalOptions'] = {
+  animationDuration: 300,
+  sizes: {
+    mobile: 480,
+    tablet: 768,
+    tabletLg: 1024,
+    desktop: 1280,
+    desktopLg: 1600,
+    desktopXl: 1920
+  }
+};
 
 $(() => {
   svg4everybody();
