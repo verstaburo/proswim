d4c8802f6117d01eb8b6e32d60ffba6d931cc274
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-19 00:42:35 +0300

    initial section markup

---
+++ app/components/products-section/products-section.js
@@ -0,0 +1,20 @@
+import * as Swiper from 'swiper/dist/js/swiper';
+
+// eslint-disable-next-line no-unused-vars
+const slider = new Swiper('.js-products-section-slider', {
+  loop: true,
+  autoplay: false,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.products-section__button_next',
+    prevEl: '.products-section__button_prev',
+  },
+  roundLengths: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+    },
+  },
+});
+
