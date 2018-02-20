be299f393a32c85b7fd8f485b1787356f946b4a8
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-20 14:54:28 +0300

    truncate

--- app/blocks/js-functions/truncate.js
+++ app/blocks/js-functions/truncate.js
@@ -1,43 +1,11 @@
 const $ = window.$;
 
-function truncate() {
-  $('[data-lines]').each(function () {
-    const self = $(this);
-    const lines = self.data('lines');
-    let linesCount = Number(lines);
-    const ww = $(window).width();
+$('.js-truncate').each(function () {
+  const self = $(this);
 
-    if (!self.is(':visible')) {
-      return;
-    }
-
-    if (typeof lines === 'object') {
-      if (ww < window.globalOptions.sizes.tablet && 'mobile' in lines) {
-        linesCount = Number(lines.mobile);
-      } else if (ww < window.globalOptions.sizes.desktop && 'tablet' in lines) {
-        linesCount = Number(lines.tablet);
-      } else {
-        linesCount = Number(lines.desktop);
-      }
-    }
-
-    const lh = parseInt(self.css('line-height'), 10);
-    const height = lh * linesCount;
-
-    self.css({
-      maxHeight: height,
-      overflow: 'hidden',
-    });
-
-    self.dotdotdot();
+  self.dotdotdot({
+    truncate: 'letter',
+    watch: 'window',
+    height: self.css('max-height'),
   });
-}
-
-truncate();
-
-let resizeTimer = null;
-
-$(window).on('resize', () => {
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(truncate, 100);
 });
