287373f43802ff14dc331dd4693f4247d687dfab
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-21 10:54:28 +0300

    Update 2018-02-21T07:54:20.083Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,6 +8,16 @@
  * ВАЖНО: Файлы просто подключаются, без транспиляции (babel) минификации, поэтому нужно писать на ES5
  * Так же доступа к блокам, которые собираются с помощью вебпака не будет.
  */
+ 
+/** registration-popup */
+$('.js-registration-popup-form').on('submit', function(e) {
+    e.preventDefault();
+    var self = $(this);
 
-
+    // if (!self.parsley().isValid()) {
+    //     return;
+    // }
+  
+    // $.fancybox.open({ src: '#registration-complete-popup' });
+});
 /* eslint-enable */
