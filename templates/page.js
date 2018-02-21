836817319baca8af6a748aaaa8a80beaa34a17d8
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-21 18:43:26 +0300

    news page desktop

--- templates/page.js
+++ templates/page.js
@@ -2,8 +2,16 @@ module.exports = pageName => `
 extends ../../layouts/default
 
 block head
-  - var pageTitle = 'Versta Project Template'
+  - var pageTitle = 'Proswim - ${pageName}'
 
 block content
   +b.page
+    .container
+      +e.head
+        +e.headcol._flexible
+          +breadcrumbs(['Главная', '${pageName}'])
+
+      +e.H1.heading ${pageName}
+
+block popups
 `;
