7ad978dafda1b0bc8078ac7c93841837e9c146b8
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-26 12:38:34 +0300

    Update 2018-02-26T09:38:22.997Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,16 +8,47 @@
  * ВАЖНО: Файлы просто подключаются, без транспиляции (babel) минификации, поэтому нужно писать на ES5
  * Так же доступа к блокам, которые собираются с помощью вебпака не будет.
  */
- 
-/** registration-popup */
-$('.js-registration-popup-form').on('submit', function(e) {
-    e.preventDefault();
-    var self = $(this);
 
-    // if (!self.parsley().isValid()) {
-    //     return;
-    // }
-  
-    // $.fancybox.open({ src: '#registration-complete-popup' });
-});
+// new Vue({
+//   data: {
+//     el: '.page',
+//     brandSearch: '',
+//     brandsList: [
+//       { title: 'Adidas', value: 'Adidas', name: 'filter-brand', checked: false },
+//       { title: 'Adl', value: 'Adl', name: 'filter-brand', checked: false },
+//       { title: 'Banana Republic', value: 'Banana Republic', name: 'filter-brand', checked: false },
+//       { title: 'Baon', value: 'Baon', name: 'filter-brand', checked: false },
+//       { title: 'Befree', value: 'Befree', name: 'filter-brand', checked: false },
+//       { title: 'Betsy', value: 'Betsy', name: 'filter-brand', checked: false },
+//       { title: 'Caprice', value: 'Caprice', name: 'filter-brand', checked: false },
+//       { title: 'United Colors Of Benneton', value: 'United Colors Of Benneton', name: 'filter-brand', checked: false },
+//     ],
+//     productList: [
+//       { title: 'Очки для тренировок', value: '1', name='filter-product', checked: false },
+//       { title: 'Профессиональные очки для плавания', value: '1', name='filter-product', checked: false },
+//       { title: 'Очки-маски', value: '1', name='filter-product', checked: false },
+//       { title: 'Очки с диоприями', value: '1', name='filter-product', checked: false },
+//       { title: 'Наборы “Очки+Шапочка”к', value: '1', name='filter-product', checked: false },
+//     ],
+//     price: { min: 0, max: 0 },
+//     sort: '',
+//   },
+//   computed: {
+//     brandsFilter: function() {
+//       return this.productList.filter(function(item) {
+//         return item.title.indexOf(this.brandSearch) !== -1;
+//       });
+//     },
+//     selectedBrand: function() {
+//       return this.brandList.filter(function(item) {
+//         return item.checked;
+//       });
+//     },
+//     selectedProducts: function() {
+//       return this.productList.filter(function(item) {
+//         return item.checked;
+//       });
+//     },
+//   },
+// });
 /* eslint-enable */
