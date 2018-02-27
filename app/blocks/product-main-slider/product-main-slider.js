3f4f9cfa203d7b54219e44cb3fec4cd424e8740f
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-27 23:15:48 +0300

    fix

--- app/blocks/product-main-slider/product-main-slider.js
+++ app/blocks/product-main-slider/product-main-slider.js
@@ -1,5 +1,7 @@
 import * as Swiper from 'swiper/dist/js/swiper';
 
+const $ = window.$;
+
 // eslint-disable-next-line no-unused-vars
 const slider = new Swiper('.js-product-main-slider', {
   slidesPerView: 1,
@@ -8,4 +10,31 @@ const slider = new Swiper('.js-product-main-slider', {
     prevEl: '.product-main-slider__button_prev',
   },
   roundLengths: true,
+});
+
+/**
+ * Ховер на картинку в большем слайдере
+ */
+
+$(document).on('mouseenter touchstart', '.js-product-main-slider-zoom', function (e) {
+  const self = $(this);
+  const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
+
+  self
+    .addClass('is-hovered')
+    .find('img')
+    .css('transform-origin', `${clientX}px ${clientY}px`);
+});
+
+$(document).on('mousemove touchmove', '.js-product-main-slider-zoom', function (e) {
+  const self = $(this);
+  const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
+
+  self
+    .find('img')
+    .css('transform-origin', `${clientX}px ${clientY}px`);
+});
+
+$(document).on('mouseleave touchend', '.js-product-main-slider-zoom', function () {
+  $(this).removeClass('is-hovered');
 });
--- app/components/product-item-section/product-item-section.js
+++ app/components/product-item-section/product-item-section.js
@@ -28,36 +28,3 @@ function productItemSection() {
 }
 
 productItemSection();
-
-/**
- * Ховер на картинку в большем слайдере
- */
-
-$(document).on('mouseenter touchstart', '.js-product-item-section .product-main-slider__slide', function (e) {
-  const self = $(this);
-  const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
-
-  self.parents('.js-product-item-section').find('.product-item-section__zoom').addClass('is-hidden');
-
-  self
-    .addClass('is-hovered')
-    .find('img')
-    .css('transform-origin', `${clientX}px ${clientY}px`);
-});
-
-$(document).on('mousemove touchmove', '.js-product-item-section .product-main-slider__slide', function (e) {
-  const self = $(this);
-  const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
-
-  self
-    .find('img')
-    .css('transform-origin', `${clientX}px ${clientY}px`);
-});
-
-$(document).on('mouseleave touchend', '.js-product-item-section .product-main-slider__slide', function () {
-  $(this)
-    .removeClass('is-hovered')
-    .parents('.js-product-item-section')
-    .find('.product-item-section__zoom')
-    .removeClass('is-hidden');
-});
