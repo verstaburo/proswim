c2836e90683ae91e81f4e575b8905cdac1bc5714
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-27 14:14:14 +0300

    product cards desktop sections

--- app/components/products-section/products-section.js
+++ app/components/products-section/products-section.js
@@ -1,21 +1,36 @@
 import * as Swiper from 'swiper/dist/js/swiper';
 
-// eslint-disable-next-line no-unused-vars
-const slider = new Swiper('.js-products-section-slider', {
-  loop: true,
-  autoplay: false,
-  slidesPerView: 4,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: '.products-section__button_next',
-    prevEl: '.products-section__button_prev',
-  },
-  roundLengths: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 'auto',
-      spaceBetween: 10,
+const $ = window.$;
+
+$('.js-products-section').each(function () {
+  const self = $(this);
+  const sliderEl = self.find('.js-products-section-slider');
+  const isSmall = self.hasClass('products-section_small');
+
+  const slidesPerView = isSmall ? 6 : 4;
+  const { lg, sm } = window.globalOptions.sizes;
+
+  // eslint-disable-next-line no-unused-vars
+  const slider = new Swiper(sliderEl, {
+    loop: true,
+    autoplay: false,
+    slidesPerView,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.products-section__button_next',
+      prevEl: '.products-section__button_prev',
     },
-  },
+    roundLengths: true,
+    breakpoints: {
+      [lg - 1]: {
+        slidesPerView: 4,
+      },
+      [sm - 1]: {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+      },
+    },
+  });
 });
 
+$(window).resize();
