3f83d8b66c8e9e3f01fb12e782cd926768ae0ddb
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-28 11:40:27 +0300

    fix slider

--- app/blocks/product-main-slider/product-main-slider.js
+++ app/blocks/product-main-slider/product-main-slider.js
@@ -36,19 +36,20 @@ $(document).on('mouseenter touchstart', '.js-product-main-slider-zoom', function
   const self = $(this);
   const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
 
-  self
+  const img = self
     .addClass('is-hovered')
-    .find('img')
-    .css('transform-origin', `${clientX}px ${clientY}px`);
+    .find('img');
+
+  img.css('transform-origin', `${clientX - img.offset().left}px ${clientY - img.offset().top}px`);
 });
 
 $(document).on('mousemove touchmove', '.js-product-main-slider-zoom', function (e) {
   const self = $(this);
   const { clientX, clientY } = e.touches && e.touches.length ? e.touches[0] : e;
 
-  self
-    .find('img')
-    .css('transform-origin', `${clientX}px ${clientY}px`);
+  const img = self.find('img');
+
+  img.css('transform-origin', `${clientX - img.offset().left}px ${clientY - img.offset().top}px`);
 });
 
 $(document).on('mouseleave touchend', '.js-product-main-slider-zoom', function () {
