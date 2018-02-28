1819a1b7853b4247ab93f18ede9a9de8fef3ff62
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-02-28 16:36:17 +0300

    product tooltips

--- app/blocks/tooltip/tooltip.js
+++ app/blocks/tooltip/tooltip.js
@@ -5,11 +5,30 @@ import $ from 'jquery';
 import tooltipster from 'tooltipster';
 
 export default function tooltips() {
-  $('.js-tooltip').tooltipster({
-    animation: 'fade',
-    delay: 0,
-    side: 'top',
-    theme: 'tooltipster-borderless',
+  $('.js-tooltip').each(function () {
+    const self = $(this);
+
+    self.tooltipster({
+      contentAsHTML: true,
+      animationDuration: window.globalOptions.animationDuration,
+      animation: 'fade',
+      delay: 0,
+      debug: true,
+      side: 'top',
+      theme: 'tooltipster-borderless',
+      functionInit(instance, helper) {
+        const $origin = $(helper.origin);
+        let dataOptions = $origin.attr('data-tooltipster');
+
+        if (dataOptions) {
+          dataOptions = JSON.parse(dataOptions);
+
+          $.each(dataOptions, (name, option) => {
+            instance.option(name, option);
+          });
+        }
+      },
+    });
   });
 }
 /* eslint-enable no-unused-vars */
