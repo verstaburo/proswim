c612c9cd28d6e6282cf2ae8bae9dc3fc438ef808
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-03-05 11:41:28 +0300

    Update 2018-03-05T08:41:20.016Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,24 @@
 /* eslint-disable */
-
+ /* eslint-disable */
 /**
- * REMOVE IT
- *
- * Скрипты можно писать тут, либо подключать с помощь https://github.com/coderhaoxin/gulp-file-include
- *
- * ВАЖНО: Файлы просто подключаются, без транспиляции (babel) минификации, поэтому нужно писать на ES5
- * Так же доступа к блокам, которые собираются с помощью вебпака не будет.
+ * #################################
+ * Отправка формы на странице товара
+ * #################################
  */
+
+$('.js-product-item-form').on('submit', function (e) {
+  e.preventDefault();
+  var self = $(this);
+
+  self.parsley().validate();
+
+  if (!self.parsley().isValid()) {
+    return;
+  }
+
+  window.popups.show('#item-added-popup');
+});
+/* eslint-enable */
 
 // new Vue({
 //   data: {
