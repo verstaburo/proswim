77b88f4b2ec7dc5d5a5137703d4ce283dbc5a553
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-03-14 15:12:16 +0300

    account-order desktop

--- app/blocks/total/total.js
+++ app/blocks/total/total.js
@@ -1,33 +1,5 @@
 const $ = window.$;
 
-$(window).on('load scroll', () => {
-  const block = $('.js-total');
-
-  if (!block.length) {
-    return;
-  }
-
-  const parentOffset = block.parent().offset().top;
-  const blockHeight = block.outerHeight();
-  const parentHeight = block.parent().height();
-
-  if (blockHeight + 10 >= parentHeight) {
-    return;
-  }
-
-  const topBreakpoint = $(window).scrollTop() - parentOffset >= 0;
-  const bottomBreakpoint = $(window).scrollTop() >= (parentOffset + parentHeight) - blockHeight;
-
-  if (bottomBreakpoint) {
-    block.removeClass('is-fixed').addClass('is-attached');
-  } else if (topBreakpoint) {
-    block.removeClass('is-attached').addClass('is-fixed');
-  } else {
-    block.removeClass('is-fixed is-attached');
-  }
-});
-
-
 $(document).on('click', '.js-total-promolink', function (e) {
   e.preventDefault();
 
