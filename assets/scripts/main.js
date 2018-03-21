1f813327a63dbec705dc68daf4af1a921cfe7d3b
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2018-03-21 18:48:34 +0300

    Update 2018-03-21T15:48:25.276Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,20 +54,41 @@ $('.js-product-item-form').on('submit', function (e) {
  * Обновление итоговой стоимости
  */
 function updateTotal() {
-  var totalBlock = $('.js-total'),
-      totalAmountEl = $('.js-total-amount'),
+  var totalBlock = $('.js-total');
+
+  if (!totalBlock.length) {
+    return;
+  }
+
+  var sumEl = totalBlock.find('.js-total-sum'),
+      deliveryEl = totalBlock.find('.js-total-delivery'),
+      amountEl = totalBlock.find('.js-total-amount'),
+      sum = 0,
       amount = 0;
 
   if (!totalBlock.length) {
     return;
   }
 
-  $('.js-product-cart-item-amount').each(function () {
-    amount += Number($(this).text().replace(/[^\d]/, ''));
+  $('.js-product-cart-item').each(function () {
+    var self = $(this);
+
+    sum += Number(self.data('item-price')) * Number(self.data('item-count'));
   });
 
-  totalAmountEl.text(window.numberWithSpaces(amount));
+  amount = sum;
+
+  if (deliveryEl.length) {
+    var deliveryPrice = Number($('.js-delivery-item:checked').data('price')) || 0;
+    amount += deliveryPrice;
+
+    deliveryEl.text(window.numberWithSpaces(deliveryPrice));
+  }
+
+  sumEl.text(window.numberWithSpaces(sum));
+  amountEl.text(window.numberWithSpaces(amount));
 }
+updateTotal();
 
 /**
  * Удаление карточки
@@ -75,7 +96,7 @@ function updateTotal() {
 $(document).on('click', '.js-product-cart-item-remove', function (e) {
   e.preventDefault();
   $(this)
-    .parents('.product-cart-item')
+    .parents('.js-product-cart-item')
     .fadeOut(window.globalOptions.animationDuration, function () {
       $(this).remove();
       updateTotal();
@@ -87,20 +108,39 @@ $(document).on('click', '.js-product-cart-item-remove', function (e) {
  */
 $(document).on('change', '.js-product-cart-item-input', function () {
   var self = $(this),
-      parent = self.parents('.product-cart-item'),
+      parent = self.parents('.js-product-cart-item'),
       priceEl = parent.find('.js-product-cart-item-price'),
       countEl = parent.find('.js-product-cart-item-count'),
       amountEl = parent.find('.js-product-cart-item-amount');
 
-  var price = Number(priceEl.text().replace(/[^\d]/, ''));
+  var price = Number(parent.data('item-price'));
       count = Number(self.val()),
       amount = price * count;
 
   amountEl.text(window.numberWithSpaces(amount));
+  parent.data('item-price', price);
+
   countEl.text(count);
+  parent.data('item-count', count);
 
   countEl.parents('.product-cart-item__col').toggleClass('is-empty', count <= 1);
   updateTotal();
+});
+
+/**
+ * Обновление стоимости доставки
+ */
+$(document).on('change', '.js-delivery-item', updateTotal);
+
+/**
+ * Выбор первой радиокноки в выбранной вкладке (доставка)
+ */
+$(document).on('TAB_SHOWN', '[data-tab]', function () {
+  var deliveryItem = $(this).find('.js-delivery-item');
+
+  if (deliveryItem.length) {
+    deliveryItem.first().prop('checked', true).trigger('change');
+  }
 });
 
 /**
@@ -121,46 +161,12 @@ function initMap() {
     accessToken: 'pk.eyJ1IjoidGhldmVydmVyeTEiLCJhIjoiY2lzZXdzaXZ4MDBjaTJudm93dDI4MGVrMCJ9.Z8KKk0M_lpDTPB6_JtJBxg',
   }).addTo(map);
 
-  var marker = window.makeMapPin([59.934, 30.335]).addTo(map),
-      marker2 = window.makeMapPin([59.931494, 30.330091]).addTo(map),
-      marker3 = window.makeMapPin([59.929947, 30.337687]).addTo(map);
-
-  marker.bindPopup(getDeliveryPopup({
-    station: 'Ленинский пр-т',
-    addr: 'Ул. Вавилова, д.3, ТЦ “Гагаринский”',
-    description: '1 минута пешком от метро, 2-й этаж',
-    textList: [
-      { key: 'График работы:', value: 'с 9.00 до 20.00' },
-      { key: 'Телефон магазина:', value: '8 (495) 730-30-62' },
-      { key: 'Примерка:', value: 'Есть примерка' },
-    ],
-  }));
-
-  marker2.bindPopup(getDeliveryPopup({
-    station: 'Ленинский пр-т',
-    addr: 'Ул. Вавилова, д.3, ТЦ “Гагаринский”',
-    description: '1 минута пешком от метро, 2-й этаж',
-    textList: [
-      { key: 'График работы:', value: 'с 9.00 до 20.00' },
-      { key: 'Телефон магазина:', value: '8 (495) 730-30-62' },
-      { key: 'Примерка:', value: 'Есть примерка' },
-    ],
-  }));
-
-  marker3.bindPopup(getDeliveryPopup({
-    station: 'Ленинский пр-т',
-    addr: 'Ул. Вавилова, д.3, ТЦ “Гагаринский”',
-    description: '1 минута пешком от метро, 2-й этаж',
-    textList: [
-      { key: 'График работы:', value: 'с 9.00 до 20.00' },
-      { key: 'Телефон магазина:', value: '8 (495) 730-30-62' },
-      { key: 'Примерка:', value: 'Есть примерка' },
-    ],
-  }));
-
+  // Обновляем размеры карты при переключении вкладки
   $(document).on('TAB_SHOWN', '[data-tab]', function () {
     map.invalidateSize(false);
   });
+
+  return map;
 }
 
 /**
@@ -172,7 +178,15 @@ function getDeliveryPopup(data) {
       station = $('<div class="address-popup__heading">' + data.station + '</div>'),
       addr = $('<p class="address-popup__text address-popup__text_bold">' + data.addr + '</p>');
       description = $('<p class="address-popup__text">' + data.description + '</p>'),
-      btn = $('<button class="button button_size_sm address-popup__button">Доставить сюда</button>');
+      btn = $('<button class="button button_size_sm address-popup__button js-select-self-discharge-point">Доставить сюда</button>');
+
+  if (data.city) {
+    popup.attr('data-city', data.city);
+  }
+
+  if (data.icon) {
+    popup.attr('data-icon', data.icon);
+  }
 
   popup
     .append(station)
@@ -192,7 +206,6 @@ function getDeliveryPopup(data) {
   body.append(btn);
 
   return L.popup({
-      closeButton: false,
       className: 'address-popup-wrapper',
       width: 288,
       offset: {
@@ -203,7 +216,113 @@ function getDeliveryPopup(data) {
     .setContent(popup[0].outerHTML);
 }
 
-initMap();
+var deliveryCartMap = initMap(),
+    delvieryCartMapMarkers = [];
+
+/**
+ * При переключении радиокнопки рисуем маркеры на карте доставки
+ */
+$(document).on('change', '.js-delivery-item', function () {
+  if (!deliveryCartMap) {
+    return;
+  }
+
+  if (!$(this).parents('.tabs-content__wrapper').find('.map').length) {
+    return;
+  }
+
+  // Удаляем маркеры
+  $.each(delvieryCartMapMarkers, function () {
+    this.remove();
+  });
+
+  // Генерация попапа
+  var popup = getDeliveryPopup({
+    station: 'Ленинский пр-т',
+    addr: 'Ул. Вавилова, д.3, ТЦ “Гагаринский”',
+    description: '1 минута пешком от метро, 2-й этаж',
+    textList: [
+      { key: 'График работы:', value: 'с 9.00 до 20.00' },
+      { key: 'Телефон магазина:', value: '8 (495) 730-30-62' },
+      { key: 'Примерка:', value: 'Есть примерка' },
+    ],
+    icon: 'logo',
+    city: 'Москва',
+  });
+
+  // генерация маркеров
+  delvieryCartMapMarkers = [
+    window.makeMapPin([59.934, 30.335]).addTo(deliveryCartMap).bindPopup(popup),
+    marker2 = window.makeMapPin([59.931494, 30.330091]).addTo(deliveryCartMap).bindPopup(popup),
+    marker3 = window.makeMapPin([59.929947, 30.337687]).addTo(deliveryCartMap).bindPopup(popup),
+  ];
+});
+
+/**
+ * Выбор пункта самовывоза
+ */
+
+$(document).on('click', '.js-select-self-discharge-point', function (e) {
+  e.preventDefault();
+
+  var block = $(this).parents('.address-popup'),
+      section = $('.js-selected-self-discharge-point');
+
+  if (!section.length) {
+    return;
+  }
+
+  var newBlock = block.clone(),
+      icon = newBlock.data('icon'),
+      city = newBlock.data('city');
+
+  // добавляем город
+  if (city) {
+    newBlock.prepend('<div class="address-popup__heading address-popup__heading_city">' + city + '</div>');
+  }
+
+  // добавляем иконку
+  if (icon) {
+    newBlock.prepend('<svg class="icon icon_' + icon + '"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/images/icon.svg#icon_' + icon + '"></use></svg>');
+  }
+
+  // меняем модификатор
+  newBlock.addClass('address-popup_chosen');
+
+  // меняем кнопку
+  newBlock
+    .find('.button')
+    .removeClass('button_size_sm js-select-self-discharge-point')
+    .addClass('button_size_md button_transparent js-select-another-self-discharge-point')
+    .text('Выбрать другой');
+
+  // Скрываем карту
+  $('.js-delivery-map-col').fadeOut(window.globalOptions.animationDuration);
+
+  // рендерим блок
+  section
+    .fadeIn(window.globalOptions.animationDuration)
+    .find('.address-popup-section__content')
+    .html(newBlock);
+
+  // Закрываем попапы
+  if (deliveryCartMap) {
+    deliveryCartMap.closePopup();
+  }
+});
+
+/**
+ * Выбор другого пункта самовывоза
+ */
+$(document).on('click', '.js-select-another-self-discharge-point', function (e) {
+  e.preventDefault();
+  // Скрываем секцию выбранного пункта
+  $('.js-selected-self-discharge-point').fadeOut(window.globalOptions.animationDuration);
+
+  // Показываем карту
+  $('.js-delivery-map-col').fadeIn(window.globalOptions.animationDuration);
+
+});
 
 /**
  * Карты на странице контактов
@@ -243,9 +362,9 @@ initContactsMap('msk-shop-contacts-map', [[59.934, 30.335], [59.9336222, 30.3469
 initContactsMap('spb-shop-contacts-map', [[59.934, 30.335]]);
 
 
- /**
-  * Показать город
-  */
+/**
+* Показать город
+*/
 
 $(document).on('change', '.js-no-city-in-list', function(e) {
   var isChecked = $(this).is(':checked'),
